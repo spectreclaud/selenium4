926f30eaf96e7a707b7418deeff15b7ffe09d949
Rafal Stalka <rafal@saveatrain.com>
2024-10-22 18:42:49 +0200

    Initial commit

---
+++ src/test/java/RelativeLocators.java
@@ -0,0 +1,47 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
+public class RelativeLocators {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testRelativeLocators() {
+        WebElement loginPanel = driver.findElement(By.id("loginPanel"));
+        WebElement credentials = driver.findElement(RelativeLocator.with(By.tagName("span")).above(loginPanel));
+        System.out.println(credentials.getText());
+    }
+
+    @Test
+    public void testListOfElements() {
+        List<WebElement> allSocialMedia = driver.findElements(with(
+                By.tagName("img"))
+                .near(By.id("footer")));
+        for (WebElement element : allSocialMedia) {
+            System.out.println(element.getAttribute("alt"));
+        }
+    }
+}
